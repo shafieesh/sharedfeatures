b560db1ef9e67afd0428ab83878731c1cb1aa616
Shayan <shafiee-sh@outlook.com>
2020-10-16 12:15:19 +0330

    update

--- src/main/java/com/chainedminds/dataClasses/BaseContentData.java
+++ src/main/java/com/chainedminds/dataClasses/BaseContentData.java
@@ -1,11 +1,8 @@
 package com.chainedminds.dataClasses;
 
-import jdk.jfr.Unsigned;
-
 public class BaseContentData {
 
     @Deprecated
-    @Unsigned
     public String id;
     @SuppressWarnings("unused")
     public int type;
@@ -14,9 +11,7 @@ public class BaseContentData {
     @SuppressWarnings("unused")
     public String link;
     @Deprecated
-    @Unsigned
     public byte[] blob;
     @Deprecated
-    @Unsigned
     public String fileName;
 }
